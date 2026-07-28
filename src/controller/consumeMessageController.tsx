0af0053b66421bca29d70fc2e8839d49ac8ddf3d
venzsupp <support@venzupp.com>
2026-07-28 22:40:51 +1200

    array function

--- src/controller/consumeMessageController.tsx
+++ src/controller/consumeMessageController.tsx
@@ -12,13 +12,13 @@ const getWeatherDataFromSqs = async(request: Request, response: Response, next:N
             WaitTimeSeconds: 20,       // Long Polling: Waits up to 20s for messages to arrive
             VisibilityTimeout: 30,     // Locks messages for 30s so others don't read them
           };
-      
+          /*
         const awsClt = await awsClient();
         const res = await awsClt.send(new ReceiveMessageCommand(receiveParams));
         if (!res.Messages || res.Messages.length === 0) {
             console.log("No messages found in this poll.");
             return;
-          }
+        }
           // console.log('===res===');
           // console.log(res);
         const httpStatus = res.$metadata.httpStatusCode;
@@ -36,14 +36,63 @@ const getWeatherDataFromSqs = async(request: Request, response: Response, next:N
             // console.log(parseBody);
             return restData.data;
         });
-        // console.log(res.$metadata.httpStatusCode);
-        // console.log(messageBodies);
-        // const {timestamp, ...restData} = messageBodies;
-        //   console.log('res.Messages');
-        //   console.log(messageBodies);
+
         const msgHttpStatus = {"messages": messageBodies, "httpStatus": httpStatus};
         console.log(msgHttpStatus);
-        return msgHttpStatus;
+        */
+
+        const msgHttpStatus = [
+            {
+                'name':'test name',
+                'temperature': 12,
+                'humidity': 2
+            },
+            {
+                'name':'name1',
+                'temperature': 10,
+                'humidity': 4
+            },
+            {
+                'name':'ud',
+                'temperature': 1,
+                'humidity': 5
+            },
+            {
+                'name':'mani',
+                'temperature': -1,
+                'humidity': 10
+            }
+        ];
+        console.log(msgHttpStatus);
+        // const newmesg = msgHttpStatus.filter(item => item.temperature > 5);
+        // const newmesg = msgHttpStatus.some(item => item.name === 'mani'); // true if found and false not found
+       // const newmesg = msgHttpStatus.every(item => item.name === 'mani'); // true if found and false not found
+        
+       // -- make array with index and value
+       // const newmesg = msgHttpStatus.entries(); 
+        // for (const [index, element] of newmesg) {
+        //     console.log(`Index: ${index}, Value: ${element.name}`);
+        // }
+
+        // ---- get array keys
+        // const newmesg = msgHttpStatus.keys(); 
+        // for (const index of newmesg) {
+        //     console.log(`Index: ${index}`);
+        // }
+
+        // console.log(newmesg);
+
+       const res =  msgHttpStatus.findLast(item => item.name === 'ud');
+       console.log(res);
+        // msgHttpStatus.reverse();
+        
+        // msgHttpStatus.sort((a,b) => a.temperature - b.temperature);
+        // msgHttpStatus.sort((a,b) => a.name.localeCompare(b.name));
+         return response.status(200).json({ 
+            success: true,
+            result: msgHttpStatus
+          });
+        // return msgHttpStatus;
         // for (const msg of res.Messages) {
         //     const body = JSON.parse(msg.Body);
         //     console.log(`Processing message sequence:`, body);
